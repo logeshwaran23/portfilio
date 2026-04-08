d887eb30b98f757b8eb100ad6b8c6c3a4c638756
Kumaravel V <kumaravelkuma07@gmail,com>
2026-04-08 19:46:53 +0530

    changes1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? ''
+const isUserOrOrgPagesRepo = repoName.toLowerCase().endsWith('.github.io')
+const githubPagesBase = isUserOrOrgPagesRepo ? '/' : `/${repoName}/`
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_ACTIONS ? githubPagesBase : '/',
 })
